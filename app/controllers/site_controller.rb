fe580dc80e3da60522e3e5c06b61b91f9e757536
pedro-santos28 <phdss04@gmail.com>
2023-11-21 08:06:55 -0300

    fix: removing db usage for changing the locale

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,11 +3,11 @@ class SiteController < ApplicationController
   before_action :set_locale
 
   def index
-    locale = Language.find_by(abbreviation: params[:locale])
+    locale =  params[:locale]
 
     if locale.present?
-      I18n.locale = locale[:abbreviation] || I18n.default_locale
-      cookies[:locale] = locale[:abbreviation]
+      I18n.locale = locale || I18n.default_locale
+      cookies[:locale] = locale
     end
   end
 end
